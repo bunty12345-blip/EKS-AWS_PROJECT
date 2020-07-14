481cfcc5b42fa289550a930fb2f0726b1d3eda41
SUJAYSHANKAR BISWAS <63721725+bunty12345-blip@users.noreply.github.com>
2020-07-14 17:39:50 +0530

    configurationclientfile

---
+++ configclientfile.php
@@ -0,0 +1,11 @@
+// Create your configuration file using php code to host one-drive server
+<?php
+
+return [ 
+               'ONEDRIVE_CLIENT_ID' => '<YOUR_CLIENT_ID>',
+
+               'ONEDRIVE_CLIENT_SECRET' => '<YOUR_CLIENT_SECRET>',
+
+               'ONEDRIVE_REDIRECT_URI' => 'https://builder.honeycode.aws/redirect.php',
+];
+
